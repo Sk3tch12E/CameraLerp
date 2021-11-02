bad85624c1bb5c3a52300ae6b2f2658da36d47c0
Sk3tch12E <maddocks.jos@gmail.com>
2021-11-02 12:21:26 +0000

    Added Files

---
+++ CamController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    List<Vector3> camPositions = new List<Vector3>();
+    public GameObject Building; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform pos in Building.GetComponentInChildren<Transform>())
+        {
+            camPositions.Add(pos.position + new Vector3(0,5,0));
+        }
+
+        foreach (Vector3 v3 in camPositions)
+        {
+            Debug.Log(v3);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            Vector3 newpos = camPositions[ Random.Range(0, camPositions.Count)];//get new random position
+                                                                                //Teleport
+                                                                                //transform.position = newpos;
+
+            //Lerp
+            StartCoroutine(StartLerp(this.transform.position, newpos, false));
+            //
+        }
+    }
+
+    IEnumerator StartLerp(Vector3 _startPos, Vector3 _endPos, bool _repeat)
+    {
+        yield return this.GetComponent<Lerper>().StartLerp(_startPos, _endPos, _repeat);
+    }
+}
---
+++ Lerper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerper : MonoBehaviour
+{
+    public GameObject gb;
+    public Vector3 startPos, endPos;
+    public bool repeatable;
+    public float speed = 1.0f;
+
+    float startTime, totalDistance;
+
+    // Start is called before the first frame update
+    public IEnumerator StartLerp(Vector3 _startPos, Vector3 _endPos, bool _repeat)
+    {
+        Debug.Log("a");
+
+        startPos = _startPos;
+        endPos = _endPos;
+        startTime = Time.time;
+        totalDistance = Vector3.Distance(startPos, endPos);
+
+        if (_repeat)
+        {
+            yield return Lerp(startPos, endPos, 0.5f);
+            yield return Lerp(endPos, startPos, 0.5f);
+        }
+        else
+        {
+            yield return Lerp(endPos, startPos, 0.5f);
+        }
+    }
+
+    private IEnumerator Lerp(Vector3 a, Vector3 b, float time)
+    {        
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+        while (i < 1.0f)
+        {
+           
+            i += Time.deltaTime * rate;
+
+            gb.transform.position = Vector3.Lerp(b, a, i);
+            yield return null;// Vector3.Lerp(a, b, i);
+        }
+    }
+}
